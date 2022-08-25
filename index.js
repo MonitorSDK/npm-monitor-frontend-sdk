6fee5ed0974323a62c397002cfabed358562fa83
Liny777 <1093742432@qq.com>
2022-08-25 16:10:06 +0800

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+import { login } from "./utils/login"
+import { injectXHR } from "./lib/xhr"
+import {injectJsError} from "./lib/jsError"
+import {injectPromiseError} from "./lib/promiseError"
+import {injectBlankScreen} from "./lib/blankError"
+import {countStayTime} from "./lib/stayTime"
+// 注册账户
+login();
+// 网络请求监控
+injectXHR();
+// js错误监控
+injectJsError();
+// promise错误监控
+injectPromiseError();
+// blankscreen error
+injectBlankScreen();
+// pageload
+countStayTime();
---
+++ utils/login.js
@@ -0,0 +1,23 @@
+// 首次执行sdk需要登录获取userid
+export function login() {
+    console.log("login----------");
+    // here is Your API To Create user'
+    let getUserAPI = 'https://monitor-backend.xiaotianxt.com/api/useraction/login'
+    let userid = localStorage.getItem('SDK_userId');
+    console.log(userid)
+    if (!userid) {
+        var httpRequest = new XMLHttpRequest();//第一步：建立所需的对象
+        httpRequest.open('GET', `${getUserAPI}?page=${window.location.href}`, true);//第二步：打开连接  将请求参数写在url中  ps:"./Ptest.php?name=test&nameone=testone"
+        httpRequest.send();//第三步：发送请求  将请求参数写在URL中
+        /**
+         * 获取数据后的处理程序
+         */
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                var json = httpRequest.responseText;//获取到json字符串，还需解析
+                let res = JSON.parse(json)
+                localStorage.setItem('SDK_userId', res.data)
+            }
+        }
+    }
+}
